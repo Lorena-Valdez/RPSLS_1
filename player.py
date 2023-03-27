23276716c477e205b941b9bac1acb5dc89ef4425
lorena valdez <lorena.valdez_nyc@hotmail.com>
2023-03-27 09:36:11 -0400

    player.py

--- player.py
+++ player.py
@@ -1,17 +1,18 @@
 class Player:
+    def __init__ (self, name):
+        self.gestures = ["Rock","Paper", "Scissors","Lizard","Spock"]
+        self.score = 0
+        self.name = name
+        self.chosen_move = ""
+        # self.player_one.name = input()
+        # self.player_two.name = input()
+        
+    def choose_move ():
+        pass
 
-    def __init__ (self, chosen_move, random_choice):
-        self.chosen_move = ()
-        self.score = ()
-        self.name = ()
-        self.move_list = ["Rock","Paper", "Scissors","Lizard","Spock"]
-
-user_action = input ("Choose one:(Rock, Paper, Scissors, Lizard, Spock): ")      
-
-class player_human (Player):
-    def __init__(self, player_human, ):
-        super().__init__(player_human)
+  
 
 
-# PARENT CLASS
-player 1 human / player 2 ai or human
+# class player_human (Player):
+#     def __init__(self, player_human, ):
+#         super().__init__(player_human)
